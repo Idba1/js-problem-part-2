4e9600b6d3759c70c6901c846520ecdab8756777
Idba1 <moniraislamself@gmail.com>
2024-02-05 04:12:56 +0600

    max3.js using funtion

--- max3.js
+++ max3.js
@@ -12,3 +12,19 @@ else {
     console.log('Kim is the kardashians boss')
 }
 
+
+
+// funtion
+function maxOfThree (num1, num2, num3){
+    if (num1 > num2 && num1 > num3) {
+        return num1;
+    }
+    else if (num2 > num1 && num2 > num3) {
+        return num2;
+    }
+    else {
+        return num3;
+    }
+}
+const max3 = maxOfThree(22, 44, 66);
+console.log('max of three is:', max3);
